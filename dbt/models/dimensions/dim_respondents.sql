096e7dd9f86dbf03e84808e97c424de57d8ef342
jeremychia <jeremy.chia@vinted.com>
2024-11-18 00:02:48 +0100

    feat: dim for respondents

---
+++ dbt/models/dimensions/dim_respondents.sql
@@ -0,0 +1,48 @@
+with
+    source as (
+        select
+            respondent_email,
+
+            respondent_work_experience,
+            respondent_bachelors,
+            respondent_awareness_analytics_engineering,
+            respondent_awareness_tools,
+            respondent_sql_level,
+
+            submitted_at
+        from {{ ref("stg_google_sheets_questionnaire_roles") }}
+    ),
+
+    renamed as (
+        select
+            respondent_email,
+            respondent_class,
+
+            respondent_work_experience as work_experience,
+            respondent_bachelors as bachelors,
+            respondent_awareness_analytics_engineering
+            as awareness_analytics_engineering,
+            respondent_awareness_tools as awareness_tools,
+            respondent_sql_level as sql_level,
+
+            submitted_at,
+            min(submitted_at) over (
+                partition by respondent_email
+            ) as first_submission_at,
+            max(submitted_at) over (partition by respondent_email) as last_updated_at
+
+        from source
+    ),
+
+    deduplicated as (
+        select * except (submitted_at)
+        from renamed
+        -- keep only latest response
+        qualify
+            row_number() over (partition by respondent_email order by submitted_at desc)
+            = 1
+
+    )
+
+select *
+from deduplicated
